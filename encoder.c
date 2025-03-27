a98e8aa76240040030824d88c1620279099a4006
ron <ronshav@post.bgu.org.il>
2025-03-27 09:39:10 +0200

    added encode.c part1

--- encoder.c
+++ encoder.c
@@ -1,12 +1,71 @@
-#include <stdio>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 
-bool debugFlad = true;
+bool debugFlag = true;
+
+
+bool isStrEqual(char* s1, char* s2)
+{
+	bool isEqual=true;//holds is s1 and s2 are equal 
+	bool isEnd = false;// holds is atleast one string has ended
+	while(isEqual==true && isEnd==false)
+	{
+		if (*s1 != *s2)
+		{
+			isEqual = false;
+		}
+		if (*s1 == '\0' || *s2 == '\0')
+		{
+			isEnd = true;
+		}
+		s1+=2;
+		s2+=2;
+
+	}
+	return isEqual;
+}
+
+
+
+
+char encode(char c)
+{
+	//TODO : ENCODE
+	return c;
+
+}
+
 
 int main(int argc, char* argv[])
 {
-	for (int i=0;i<10;i++)
+	char currChar = ' ';
+	for (int i=1;i<argc;i++)
 	{
-		fprintf("%d st",i);
+		if(isStrEqual(argv[i],"-d")==true)// check if thee is a "-d" flag to turn debug off
+		{
+			debugFlag=false;
+		}
+
+		if(isStrEqual(argv[i],"+d")==true)// check if thee is a "+d" flag to turn debug on
+		{
+			debugFlag=true;
+		}
+	}
+
+	for (int i=1;i<argc;i++)
+	{
+		if(debugFlag==true)
+		{
+			fprintf(stderr,"%s\n",argv[i]);
+		}
+	}
+	currChar = fgetc(stdin);
+	while(currChar != '\0')
+	{
+		fputc(encode(currChar),stdout);
+		currChar = fgetc(stdin);
 	}
 	return 0;
 }
+
--- makefile
+++ makefile
@@ -1,6 +1,5 @@
-my_echo: my_echo.c my_echo.h
-	gcc -c -g  -Wall -m32 -o  my_echo.o my_echo.c
-	gcc -g -Wall -m32 -o my_echo my_echo.o
+encoder: encoder.c
+	gcc -g -Wall -m32 -o encoder encoder.c
 
 clean: 
-	rm my_echo.o my_echo
+	rm encoder
